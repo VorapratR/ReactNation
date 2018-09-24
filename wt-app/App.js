ade8c9c2c4135a5e6f77645046dd0f6120f10bde
VorapratR <vorapratrattana@gmail.com>
2018-09-24 10:30:29 +0700

    สร้าง Forcast.js

--- wt-app/App.js
+++ wt-app/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Weather from './components/Weather';
+import Forcast from './components/Forcast';
 export default class App extends React.Component {
   render() {
     return (
-      <Weather zipCode="90110"/>
+      <Forcast main="Voraprat" dascription="5910110288" temp="25"/>
     );
   }
 }
---
+++ wt-app/components/Forcast.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Forcast extends React.Component {
+  doIt =()=> {
+    console.log("Hello from console")
+  }
+  render() {
+    return (
+      <View>
+        <Text>{this.props.main}</Text>
+        <Text>{this.props.dascription}</Text>
+        <Text>{this.props.temp}</Text>
+        <Text>C</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
