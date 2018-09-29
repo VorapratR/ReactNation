93f44d331e23efa4c61cec554bac7da0bd49b40c
VorapratR <vorapratrattana@gmail.com>
2018-09-29 13:32:04 +0700

    Route Parameter

--- wt-app/components/WeatherScreen.js
+++ wt-app/components/WeatherScreen.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import {StyleSheet, Text, Button} from 'react-native';
 import Weather from './Weather';
-const _keyExtractor = item => item.code;
-export default class WeatherScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerTitle: (<Text>Weather</Text>),
+
+export default class WeatherScreen extends React.Component{
+    static navigationOptions = ({navigation}) => {
+        return {
+            headerTitle : (<Text style = {styles.headerText}>Weather</Text>),
+            headerRight : (
+                <Button title = "Change zip" onPress = {() => navigation.navigate('ZipCode')}/>
+            )
+        }
     }
-  }
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-        <Weather  zipCode="90110"/>
-    );
-  }
+
+    render(){
+        const zipCode = this.props.navigation.getParam('zipCode')
+        return (
+            <Weather zipCode={zipCode}/>
+        );
+    }
 }
+
+
+const styles = StyleSheet.create({
+    headerText: {
+      alignItems : 'center',
+      justifyContent : 'center',
+
+    },
+  });
+  
